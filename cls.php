c96bc96407e1feb507bdeb4078bc147804354e75
jijosh <jijosh5830@gmail.com>
2016-10-13 15:29:07 +0530

    first commit

---
+++ cls.php
@@ -0,0 +1,30 @@
+
+//Great work, now we can add some properties to our class.
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Reconstructing the Person Class</title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <p>
+        <!-- Your code here -->
+        <?php 
+        class person{
+            public $isalive=true;
+            public $firstname;
+            public $lastname;
+            public $age;
+            public $teacher;
+            public $student;
+        }
+        
+      $x= new person();
+      $y= new person();
+      echo $x->isalive;
+      
+      </p>
+    </body>
+</html>
